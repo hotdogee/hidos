3029f8fbe81914b6829696b8e72ad09c64f7c9b1
Han Lin <hotdogee@gmail.com>
2016-02-04 18:24:22 +0800

    Updated version string to 0.6.1

--- hidos/hidos/app/views.py
+++ hidos/hidos/app/views.py
@@ -20,7 +20,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 
-version = '0.6'
+version = '0.6.1'
 
 def tasks(request):
     """returns task list for logged in user"""
