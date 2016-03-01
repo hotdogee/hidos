7a6385bf9da225f93fab0f9691933dd174aa69ee
Han Lin <hotdogee@gmail.com>
2016-03-01 17:55:18 +0800

    Added CellM app

---
+++ hidos/hidos/cellm/bin/1_1_CellM_han.R
@@ -0,0 +1,52 @@
+#setwd('C:\\Han\\Work\\Meridigen\\hidos\\tools\\1_6_obj_area_cal')
+# "C:\Program Files\R\R-3.2.2\bin\RScript.exe" 1_1_CellM_han.R "171-1 40x_c005_24.96.JPG" "171-1 40x_c005_24.96_out.JPG" "171-1 40x_c005_24.96.json"
+library("stats")
+library("graphics")
+library("grDevices")
+library("utils")
+library("datasets")
+library("methods")
+library("base")
+library("EBImage")
+library("RJSONIO")
+
+args <- commandArgs(TRUE)
+
+image_file_path = args[1]
+
+image_file = file.path(image_file_path)
+imgc = readImage(image_file)
+imgcs = resize(imgc, 1024)
+
+imgc_data = imageData(imgc)
+
+#--------------------------------------
+dimens = dim(imgc_data)
+imgc_data_var = matrix(0,dimens[1],dimens[2])
+for (i in 1:dimens[1] ) {
+  for (j in 1:dimens[2] ) {
+    imgc_data_var[i,j] = sd(imgc_data[i,j,])
+  }
+}
+thr = 0.05
+#-----------------------------------------
+img_binary <- ifelse(imgc_data_var<thr,1,0)
+
+kern = makeBrush(3, shape='disc')
+nuct3 = dilate(img_binary, kern)
+
+kern = makeBrush(3, shape='disc')
+nuct4 = erode(nuct3, kern)
+
+nuclabel3 = bwlabel(nuct4)
+nuclabel3_gray<-channel(nuclabel3,"gray")
+ratio = length(nuclabel3[nuclabel3<1])/length(nuclabel3)
+
+img_processed = paintObjects(nuclabel3_gray, imgc, col='#ff0000', opac=c(1, 0.4), thick=TRUE, closed=TRUE)
+file_name_output = args[2]
+writeImage(img_processed,file_name_output, quality=100)
+
+write(toJSON(list(ratio = ratio)),file = args[3])
+
+
+
---
+++ hidos/hidos/cellm/views.py
@@ -0,0 +1,166 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpRequest
+from django.http import JsonResponse
+from datetime import datetime
+from .models import CellMAnalysis
+from django.http.response import HttpResponse
+from django.conf import settings
+from os import path, makedirs, chmod
+from uuid import uuid4
+from .tasks import run_cellm_task
+import stat as Perm
+import json
+import hashlib
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+version = '0.1'
+
+def tasks(request):
+    """returns task list for logged in user"""
+    if not request.user.is_authenticated():
+        return JsonResponse({})
+    else:
+        current_tz = timezone.get_current_timezone()
+        return JsonResponse({
+            'data': [{
+                'id': t.task_id,
+                'name': t.user_filename,
+                'url': reverse('retrieve', kwargs={'task_id': t.task_id}),
+                'result_img': settings.MEDIA_URL + 'cellm/task/' + t.task_id + '/' + t.task_id + '_out.jpg',
+                'input_img': settings.MEDIA_URL + 'cellm/task/' + t.task_id + '/' + t.task_id + '_in.jpg',
+                'result': json.loads(t.result or '{}'),
+                'result_status': t.result_status,
+                'created': current_tz.normalize(t.enqueue_date.astimezone(current_tz)).isoformat(),
+                } for t in CellMAnalysis.objects.filter(user=request.user)]
+            })
+
+def status(request):
+    task_ids = []
+    if request.method == 'GET':
+        task_ids = request.GET.getlist('id')
+    elif request.method == 'POST':
+        task_ids = request.POST.getlist('id')
+    else:
+        return JsonResponse({})
+    if not task_ids:
+        return JsonResponse({})
+    return JsonResponse({
+        'data': [{
+            'id': t.task_id,
+            'result_status': t.result_status,
+            } for t in CellMAnalysis.objects.filter(task_id__in=task_ids)]
+        })
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    if request.method == 'GET':
+        return render(
+            request,
+            'cellm/index.html',
+            {
+                'title':'Home',
+                'year':datetime.now().year,
+                'version': version,
+            }
+        )
+    elif request.method == 'POST':
+        # error detection
+        if 'file' not in request.FILES:
+            return HttpResponse('Invalid file')
+        uploaded_file = request.FILES['file']
+        # calculate file hash
+        m = hashlib.md5()
+        m.update(version)
+        if request.user.is_authenticated():
+            m.update(request.user.username)
+        for chunk in uploaded_file.chunks():
+            m.update(chunk)
+        task_id = m.hexdigest()
+        # check database for duplicate
+        if not CellMAnalysis.objects.filter(task_id=task_id).exists():
+            # setup file paths
+            #task_id = uuid4().hex # TODO: Create from hash of input to check for duplicate inputs
+            path_prefix = path.join(settings.MEDIA_ROOT, 'cellm', 'task', task_id, task_id)
+            input_image_path = path_prefix + '_in.jpg'
+            output_image_path = path_prefix + '_out.jpg'
+            output_json_path = path_prefix + '_out.json'
+
+            if not path.exists(path.dirname(input_image_path)):
+                makedirs(path.dirname(input_image_path))
+            chmod(path.dirname(input_image_path), Perm.S_IRWXU | Perm.S_IRWXG | Perm.S_IRWXO) # ensure the standalone dequeuing process can open files in the directory
+        
+            # write query to file
+            if 'file' in request.FILES:
+                with open(input_image_path, 'wb') as input_image_f:
+                    for chunk in uploaded_file.chunks():
+                        input_image_f.write(chunk)
+            chmod(input_image_path, Perm.S_IRWXU | Perm.S_IRWXG | Perm.S_IRWXO) # ensure the standalone dequeuing process can access the file
+
+            # build command
+            # set R_Script="C:\Program Files\R\R-3.2.2\bin\RScript.exe"
+            # "C:\Program Files\R\R-3.2.2\bin\RScript.exe" 1_1_CellM_han.R "171-1 40x_c005_24.96.JPG" "171-1 40x_c005_24.96_out.JPG" "171-1 40x_c005_24.96.json"
+            script_path = path.join(settings.PROJECT_ROOT, 'cellm', 'bin', '1_1_CellM_han.R')
+            args_list = [[settings.R_SCRIPT, script_path, input_image_path, output_image_path, output_json_path]]
+        
+            # insert entry into database
+            record = CellMAnalysis()
+            record.task_id = task_id
+            record.version = version
+            record.user_filename = uploaded_file.name
+            record.result_status = 'queued'
+            if request.user.is_authenticated():
+                record.user = request.user
+            record.save()
+
+            run_cellm_task.delay(task_id, args_list, path_prefix)
+
+        # debug
+        #run_cellm_task.delay(task_id, args_list, path_prefix).get()
+        return HttpResponse(task_id)
+
+def retrieve(request, task_id='1'):
+    #return HttpResponse("retrieve = %s." % (task_id))
+    try:
+        record = CellMAnalysis.objects.get(task_id=task_id)
+        return render(
+            request,
+            'cellm/result.html',
+            {
+                'title': 'CellM - {0}'.format(record.user_filename),
+                'year': datetime.now().year,
+                'version': record.version,
+                'user_filename': record.user_filename,
+            }
+        )
+    except ObjectDoesNotExist:
+        message = 'Result not found'
+        return render(
+            request,
+            'cellm/error.html',
+            {
+                'title': 'CellM Error',
+                'year': datetime.now().year,
+                'version': version,
+                'message': message,
+            }
+        )
+    except Exception as e:
+        message = 'Result not found ({0})'.format(e.message)
+        return render(
+            request,
+            'cellm/error.html',
+            {
+                'title': 'CellM Error',
+                'year': datetime.now().year,
+                'version': version,
+                'message': message,
+            }
+        )
