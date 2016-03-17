ba4327c669716c155c2407ebcb90f1e24ac012c5
Han Lin <hotdogee@gmail.com>
2016-03-18 00:49:42 +0800

    Redirect runserver and celery stdout and stderr to file

--- hidos/run_celery.sh
+++ hidos/run_celery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ve
 . hidos/env/bin/activate
 cd hidos
-celery -A hidos worker --loglevel=info -n cellm.%h &
+celery -A hidos worker --loglevel=info -n cellm.%h > ../celeryout.log 2> ../celeryerr.log &
 cd ..
 deactivate
--- hidos/run_server.sh
+++ hidos/run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ve
 . hidos/env/bin/activate
 cd hidos
-python manage.py runserver 0.0.0.0:8001 &
+python manage.py runserver 0.0.0.0:8001 > ../runserverout.log 2> ../runservererr.log &
 cd ..
 deactivate
