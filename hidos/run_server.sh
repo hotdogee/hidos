cb7acbb461a86cef47d1c3af4d9b0e2cedc3c893
Han Lin <hotdogee@gmail.com>
2016-03-18 19:29:38 +0800

    Added reset data and db scripts

--- hidos/run_server.sh
+++ hidos/run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ve
 . hidos/env/bin/activate
 cd hidos
-python ./hidos/manage.py runserver 0.0.0.0:8000 > ../../log/runserverout.log 2> ../../log/runservererr.log &
+python manage.py runserver 0.0.0.0:8000 > ../../log/runserverout.log 2> ../../log/runservererr.log &
 cd ..
 deactivate
