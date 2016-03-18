ffdf7fcf11db23ac2333b899f5a184b33f4121b3
Han Lin <hotdogee@gmail.com>
2016-03-18 21:18:21 +0800

    Specify queue and host name in celery worker script.

--- hidos/run_celery.cmd
+++ hidos/run_celery.cmd
@@ -1,5 +1,5 @@
 call ./hidos/env/Scripts/activate
 cd hidos
-celery -A hidos worker --loglevel=info --concurrency=2
+celery -A hidos worker --loglevel=info --concurrency=2 -Q app -n cellc.%h
 cd ..
 call deactivate
