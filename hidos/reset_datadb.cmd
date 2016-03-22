2611cd46a2db2b44c1bb43778868f8f22eb6c9fd
Han Lin <hotdogee@gmail.com>
2016-03-22 19:43:18 +0800

    Fixed resetdb scripts

--- hidos/reset_datadb.cmd
+++ hidos/reset_datadb.cmd
@@ -1,7 +1,7 @@
 call ./hidos/env/Scripts/activate
 cd hidos
 del db.sqlite3
-rd /s /q media\image_analysis\task
+rd /s /q media\cellq\task
 python manage.py migrate
 python manage.py loaddata ../users.json
 cd ..
--- hidos/reset_datadb.sh
+++ hidos/reset_datadb.sh
@@ -2,7 +2,7 @@
 . hidos/env/bin/activate
 cd hidos
 rm -f db.sqlite3
-rm -rf media/image_analysis/task/*
+rm -rf media/cellq/task/*
 python manage.py migrate
 python manage.py loaddata ../users.json
 cd ..
