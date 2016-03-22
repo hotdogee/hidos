55d04db8fafaa8826abd8f4fd7377675f54b9f36
Han Lin <hotdogee@gmail.com>
2016-03-22 19:55:48 +0800

    User port 8002

--- hidos/run_server.sh
+++ hidos/run_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ve
 . hidos/env/bin/activate
 cd hidos
-python manage.py runserver 0.0.0.0:8001 > ../../log/runserverout.log 2> ../../log/runservererr.log &
+python manage.py runserver 0.0.0.0:8002 > ../../log/runserverout.log 2> ../../log/runservererr.log &
 cd ..
 deactivate
