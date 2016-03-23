8451b5e5324cccc5d6adc4feaa0e7c616ec84878
Han Lin <hotdogee@gmail.com>
2016-03-24 05:13:53 +0800

    Fixed kill server port

--- hidos/kill_devserver.sh
+++ hidos/kill_devserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -ve
-ps auxww | grep manage.py | grep -e "8000" | grep -v "grep" | awk '{print $2}' | xargs kill -9
+ps auxww | grep manage.py | grep -e "8002" | grep -v "grep" | awk '{print $2}' | xargs kill -9
