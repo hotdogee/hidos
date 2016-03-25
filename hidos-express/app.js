e125ecc1ef8c5473fa90f5f8814ba87e8a68af22
Han Lin <hotdogee@gmail.com>
2016-03-25 18:02:09 +0800

    Use Helmet

--- hidos-express/app.js
+++ hidos-express/app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var helmet = require('helmet');
+
+app.use(helmet());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
