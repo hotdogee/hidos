284600106b722756e43d4d893a2297817a09c936
Han Lin <hotdogee@gmail.com>
2016-04-05 06:39:49 +0800

    Added IndexView

--- hidos-django/hidos/cellc2/views.py
+++ hidos-django/hidos/cellc2/views.py
@@ -1,3 +1,15 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
-# Create your views here.
+from vanilla import TemplateView
+
+from . import app_name, verbose_name
+
+class IndexView(TemplateView):
+    template_name = 'cellbase/index.html'
+    def get_context_data(self, **context):
+        context['title'] = verbose_name
+        context['year'] = datetime.now().year
+        context['version'] = version
+        return context
