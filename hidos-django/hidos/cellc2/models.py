6764169f594ddcaf08bf6b4f8f4388be6a866b44
Han Lin <hotdogee@gmail.com>
2016-04-09 01:42:42 +0800

    Added ViewableQuerySet

--- hidos-django/hidos/cellc2/models.py
+++ hidos-django/hidos/cellc2/models.py
@@ -6,8 +6,15 @@ from . import app_name, verbose_name
 from cellbase.models import CellTaskModel
 
 
+class ViewableQuerySet(models.query.QuerySet):
+    def viewable_by(self, user):
+        return self.filter(user=user)
+
+
 class CellC2Task(CellTaskModel):
     cell_ratio = models.FloatField(null=True, blank=True)
+
+    objects = ViewableQuerySet.as_manager()
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'task_id': self.task_id}, current_app=app_name)
