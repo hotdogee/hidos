0c3a18f5db2b96d18c93ecde019c46ac27f8c97b
Weiweiann <stargazer.wang@gmail.com>
2016-04-12 18:09:34 +0800

    new homepage template

--- hidos/app/views.py
+++ hidos/app/views.py
@@ -7,6 +7,6 @@ from django.shortcuts import render, redirect
 version = '0.2.0'
 
 def home(request):
-    return redirect('/icsi')
-    # return render(request, 'app/homepage.html', {'title': 'Home', 'version': version})
+    #return redirect('/icsi')
+    return render(request, 'app/homepage.html', {'title': 'Home', 'version': version})
 
