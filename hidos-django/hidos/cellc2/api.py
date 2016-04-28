10f883596c7259a2c8cdff4819fe408dc870e875
Han Lin <hotdogee@gmail.com>
2016-04-28 19:23:20 +0800

    Changed TaskListCreateAPIView to TaskViewSet

--- hidos-django/hidos/cellc2/api.py
+++ hidos-django/hidos/cellc2/api.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from .models import CellC2Task
 from .serializers import CellC2TaskSerializer
 
-class TaskListCreateAPIView(generics.ListCreateAPIView):
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = CellC2Task.objects.all()
     serializer_class = CellC2TaskSerializer
     lookup_field = 'task_id'
