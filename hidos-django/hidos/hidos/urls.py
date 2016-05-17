70b89f98fbc8b8a09094db0f19053c8eac33fb1b
Han Lin <hotdogee@gmail.com>
2016-05-17 18:12:32 +0800

    admin urls need slashes

--- hidos-django/hidos/hidos/urls.py
+++ hidos-django/hidos/hidos/urls.py
@@ -52,10 +52,10 @@ urlpatterns = [
     url(r'^', include('cell.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
 
 # Serving files uploaded by a user during development
