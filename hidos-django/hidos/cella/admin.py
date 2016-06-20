82d2fd19372b9fcfbeb15aa5aa5b688ee3b9f18c
weian <weian.wang@meridigen.com>
2016-06-20 17:11:42 +0800

    cella admin page

--- hidos-django/hidos/cella/admin.py
+++ hidos-django/hidos/cella/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CellATask
 
-# Register your models here.
+@admin.register(CellATask)
+class CellATaskAdmin(admin.ModelAdmin):
+    date_hierarchy = 'modified'
+    fields = ('task_id', 'status', 'user', 'parent_folder', 'version',
+            'extremity','junction','mesh','total_branch_length','total_segment_length','total_network_length','total_mesh_area',
+            'uploaded_filename', 'uploaded_filetype', 'stdout', 'stderr',
+            'dequeued', 'finished',
+            'created', 'modified')
+    readonly_fields = ('created', 'modified')
+    search_fields = ['task_id', 'status', 'user', 'uploaded_filename']
+    view_on_site = True
+    list_display = ('__unicode__', 'status', 'user', 'modified', 'original_image')
+    list_filter = ('status','user')
