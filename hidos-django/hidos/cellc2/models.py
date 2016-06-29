12cbc0e18ed1bd9e8d94c23524a360b5d56c0f2c
Han Lin <hotdogee@gmail.com>
2016-06-29 19:15:02 +0800

    Refactored CellC2Task

--- hidos-django/hidos/cellc2/models.py
+++ hidos-django/hidos/cellc2/models.py
@@ -6,10 +6,10 @@ from django.conf import settings
 from django.core.urlresolvers import reverse
 
 from cell.models import File, CellTask, ViewableQuerySet, SingleImageUploadManager
-from .tasks import run_cell_c2_task
+from .tasks import process_image
 from . import app_name, verbose_name, version
 
-class CellC2Task(CellTask):
+class Task(CellTask):
     cell_ratio = models.FloatField(null=True, blank=True)
     count_min = models.FloatField(null=True, blank=True)
     count_max = models.FloatField(null=True, blank=True)
@@ -23,19 +23,7 @@ class CellC2Task(CellTask):
     def enqueue(self):
         """Insert task into task queue
         """
-        # Generate args_list and path_prefix
-        path_prefix = path.join(settings.MEDIA_ROOT, 'cellc2', 'task', self.id, self.id)
-        script_path = path.join(settings.PROJECT_ROOT, 'cellc2', 'bin', '1_6_obj_area_cal_cmd.R')
-        # avoid exploits, don't any part of the user filename
-        uploaded_image_path = path_prefix + '_uploaded.' + self.uploaded_filetype
-        result_image_path = path_prefix + '_result.' + self.uploaded_filetype
-        result_json_path = path_prefix + '_result.json'
-        # jpg image for viewer
-        input_image_viewer_path = path_prefix + '_in.jpg'
-        output_image_viewer_path = path_prefix + '_out.jpg'
-        # build command
-        args_list = [[settings.R_SCRIPT, script_path, uploaded_image_path, result_image_path, result_json_path]]
-        run_cell_c2_task.delay(self.id, args_list, path_prefix)
+        process_image.delay(self.id)
 
     # def save(self, force_insert=False, force_update=False, using=None, update_fields=None)
     # def save(self, *args, **kwargs):
