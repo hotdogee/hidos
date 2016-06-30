dfd82fd25f4a0ec0a43c7c3141ba37c56060299f
Han Lin <hotdogee@gmail.com>
2016-06-30 18:39:21 +0800

    Also install pre compiled numpy on windows

--- hidos-django/init_venv.cmd
+++ hidos-django/init_venv.cmd
@@ -1,6 +1,7 @@
 cd hidos
 virtualenv env
 call .\env\Scripts\activate
+pip install ..\lib\win64\numpy-1.11.1+mkl-cp27-cp27m-win_amd64.whl
 pip install ..\lib\win64\scipy-0.17.1-cp27-cp27m-win_amd64.whl
 pip install -r requirements.txt
 rem install opencv
