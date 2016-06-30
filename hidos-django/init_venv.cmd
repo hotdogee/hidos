eefd7f0e076afa318fa48567bcb6813572f93fb8
Han Lin <hotdogee@gmail.com>
2016-06-30 18:29:58 +0800

    Install precompiled scipy on windows first

--- hidos-django/init_venv.cmd
+++ hidos-django/init_venv.cmd
@@ -1,8 +1,9 @@
 cd hidos
 virtualenv env
 call .\env\Scripts\activate
+pip install ..\lib\win64\scipy-0.17.1-cp27-cp27m-win_amd64.whl
 pip install -r requirements.txt
 rem install opencv
-copy ..\cv\win64\cv2.pyd .\env\Lib\site-packages\cv2.pyd
+copy ..\lib\win64\cv2.pyd .\env\Lib\site-packages\cv2.pyd
 call deactivate
 cd ..
