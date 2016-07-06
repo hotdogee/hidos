3b2798065e2956c958ef70dbdce7666b173dfb95
王偉安 <weian@myhost.local>
2016-07-06 16:59:59 +0800

    set static file path

--- hidos-django/hidos/hidos/settings.py
+++ hidos-django/hidos/hidos/settings.py
@@ -137,8 +137,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
-
+STATIC_ROOT = path.join(PROJECT_ROOT, "static/")
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
