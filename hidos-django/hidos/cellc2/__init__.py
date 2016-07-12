36855653cfbd779fa2a1663ae8eb3108f0f9c084
Han Lin <hotdogee@gmail.com>
2016-07-12 17:56:36 +0800

    Move TaskAdmin to cell base app

--- hidos-django/hidos/cellc2/__init__.py
+++ hidos-django/hidos/cellc2/__init__.py
@@ -5,4 +5,4 @@ Package for the cellc2.
 version = '0.8'
 app_name = __name__ # cellc2
 verbose_name = 'Cell C2'
-default_app_config = app_name + '.apps.CellC2Config'
+default_app_config = app_name + '.apps.Config'
--- hidos-django/hidos/cellc2/admin.py
+++ hidos-django/hidos/cellc2/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 
-from .models import Task
+from cell.admin import TaskAdmin
 
-class TaskAdmin(admin.ModelAdmin):
-    date_hierarchy = 'modified'
-    fields = ('id', 'status', 'owner', 'parent_folder', 'version',
-            'result',
-            'name', 'uploaded_filetype', 'stdout', 'stderr',
-            'dequeued', 'finished',
-            'created', 'modified')
-    readonly_fields = ('created', 'modified')
-    search_fields = ['id', 'status', 'owner', 'name']
-    view_on_site = True
-    list_display = ('__unicode__', 'result', 'status', 'owner', 'modified')
+from .models import Task
 
 admin.site.register(Task, TaskAdmin)
