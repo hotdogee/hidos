f0a20f8f1481e7092b885987b6a5510f2cbcf706
Han Lin <hotdogee@gmail.com>
2016-07-15 17:09:23 +0800

    Added comments for how to stop the app

--- hidos-django/run_celery.cmd
+++ hidos-django/run_celery.cmd
@@ -1,5 +1,6 @@
 call ./hidos/env/Scripts/activate
 cd hidos
+rem Use CTRL+BREAK to interrupt the application.
 start /w /b celery -A hidos worker --loglevel=info --concurrency=2 -Q cell -n cell.%h
 cd ..
 call deactivate
--- hidos-django/run_server.cmd
+++ hidos-django/run_server.cmd
@@ -1,5 +1,6 @@
 call ./hidos/env/Scripts/activate
 cd hidos
+rem Use CTRL+BREAK to interrupt the application.
 start /w /b python manage.py runserver
 cd ..
 call deactivate
