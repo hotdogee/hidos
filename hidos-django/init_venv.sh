afce30dc6260b4c893b9da81d0cd0f0c39cf9277
Han Lin <hotdogee@gmail.com>
2016-07-31 17:06:10 +0800

    Update init venv scripts to use new local requirements file

--- hidos-django/init_venv.sh
+++ hidos-django/init_venv.sh
@@ -2,7 +2,7 @@
 cd hidos
 virtualenv env
 ./env/bin/activate
-pip install -r requirements.txt
+pip install -r requirements/local.txt
 # install opencv
 ln -s /usr/local/lib/python2.7/site-packages/cv2.so ./env/lib/python2.7/site-packages/cv2.so
 deactivate
--- hidos-django/init_venv_dev.cmd
+++
@@ -1,6 +0,0 @@
-cd hidos
-virtualenv env
-call .\env\Scripts\activate
-pip install -r dev-requirements.txt
-call deactivate
-cd ..
