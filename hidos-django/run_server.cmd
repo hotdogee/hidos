d84ed6be94ee1c6235fe6ee9bca6bbb4ff7f807c
Han Lin <hotdogee@gmail.com>
2016-08-02 19:20:43 +0800

    Add port to run_server script

--- hidos-django/run_server.cmd
+++ hidos-django/run_server.cmd
@@ -1,6 +1,6 @@
 call ./hidos/env/Scripts/activate
 cd hidos
 rem Use CTRL+BREAK to interrupt the application.
-start /w /b python manage.py runserver
+start /w /b python manage.py runserver 8000
 cd ..
 call deactivate
