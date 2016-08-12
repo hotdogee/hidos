8230b5852ff86e270630ce872b8dbe4af2b805f9
Han Lin <hotdogee@gmail.com>
2016-08-12 19:45:46 +0800

    Combined folder and file APIs

--- hidos-django/hidos/fs/urls.py
+++ hidos-django/hidos/fs/urls.py
@@ -9,25 +9,25 @@ from . import app_name # application namespace
 # url(r'^api/v1/fs/', include('fs.urls', namespace='fs')),
 
 urlpatterns = [
-    # {% url "fs:folders" %}
-    url(r'^folders$', api.FolderViewSet.as_view({
-        'get': 'list', # list user root files, maybe redundent?
-        'post': 'create',
-    }), name='folder-list'),
-    # {% url "fs:folders" folder.id %}
-    url(r'^folders/(?P<pk>[0-9a-zA-Z]+)$', api.FolderViewSet.as_view({
-        'get': 'retrieve', # includes content list
-        'put': 'update',
-        'patch': 'partial_update',
-        'delete': 'destroy'
-    }), name='folder-detail'),
+    # # {% url "fs:folders" %}
+    # url(r'^folders$', api.FolderViewSet.as_view({
+    #     'get': 'list', # list user root files, maybe redundent?
+    #     'post': 'create',
+    # }), name='folder-list'),
+    # # {% url "fs:folders" folder.id %}
+    # url(r'^folders/(?P<pk>[0-9a-zA-Z]+)$', api.FolderViewSet.as_view({
+    #     'get': 'retrieve', # includes content list
+    #     'put': 'update',
+    #     'patch': 'partial_update',
+    #     'delete': 'destroy'
+    # }), name='folder-detail'),
     # {% url "fs:files" %}
-    url(r'^files$', api.FileViewSet.as_view({
+    url(r'^$', api.FileViewSet.as_view({
         'get': 'list',
         'post': 'create'
     }), name='file-list'),
     # {% url "fs:files" file.id %}
-    url(r'^files/(?P<pk>[0-9a-zA-Z]+)$', api.FileViewSet.as_view({
+    url(r'^/(?P<pk>[0-9a-zA-Z]+)$', api.FileViewSet.as_view({
         'get': 'retrieve',
         'put': 'update',
         'patch': 'partial_update',
