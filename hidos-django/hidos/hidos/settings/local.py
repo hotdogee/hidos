f32605650fb25d611d23572d467ce25a555eff3a
Han Lin <hotdogee@gmail.com>
2016-08-15 17:24:59 +0800

    Fixed local settings

--- hidos-django/hidos/hidos/settings/local.py
+++ hidos-django/hidos/hidos/settings/local.py
@@ -27,18 +27,7 @@ INSTALLED_APPS = INSTALLED_APPS + [
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # REST framework
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework.throttling.AnonRateThrottle',
-        'rest_framework.throttling.ScopedRateThrottle',
-    ),
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/h',
-        'register_view':'20/h', # rest_auth.register.views.RegisterView
-    }
+REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {
+    'anon': '100/h',
+    'register_view':'20/h', # rest_auth.register.views.RegisterView
 }
