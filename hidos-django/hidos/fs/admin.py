0fdbb1898e20a74cde1df94298763bdfc409bbea
Han Lin <hotdogee@gmail.com>
2016-08-18 04:17:54 +0800

    Added fs admin

--- hidos-django/hidos/fs/admin.py
+++ hidos-django/hidos/fs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import File
+
+class FileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'modified'
+    fields = ('id', 'owner', 'folder', 'name', 'type', 'content_type', 'content_id',
+            'created', 'modified')
+    readonly_fields = ('id', 'created', 'modified')
+    search_fields = ['id', 'owner', 'type', 'name']
+    list_filter = ('owner', 'content_type')
+    list_display = ('__unicode__', 'type', 'folder', 'owner', 'modified')
+
+admin.site.register(File, FileAdmin)
