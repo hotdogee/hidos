e6bc96f821af9f9ac45d888788c00546fcce6a12
Han Lin <hotdogee@gmail.com>
2016-08-18 02:30:09 +0800

    Added custom JWT payload handler

---
+++ hidos-django/hidos/users/utils.py
@@ -0,0 +1,31 @@
+from calendar import timegm
+from datetime import datetime
+
+from rest_framework_jwt.compat import get_username, get_username_field
+from rest_framework_jwt.settings import api_settings
+
+
+def jwt_payload_handler(user):
+    username_field = get_username_field()
+    username = get_username(user)
+
+    payload = {
+        'email': username,
+        'full_name': user.get_full_name(),
+        'exp': datetime.utcnow() + api_settings.JWT_EXPIRATION_DELTA
+    }
+
+    # Include original issued at time for a brand new token,
+    # to allow token refresh
+    if api_settings.JWT_ALLOW_REFRESH:
+        payload['orig_iat'] = timegm(
+            datetime.utcnow().utctimetuple()
+        )
+
+    if api_settings.JWT_AUDIENCE is not None:
+        payload['aud'] = api_settings.JWT_AUDIENCE
+
+    if api_settings.JWT_ISSUER is not None:
+        payload['iss'] = api_settings.JWT_ISSUER
+
+    return payload
