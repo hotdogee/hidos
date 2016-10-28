3667f7974e9890f293868f047a7a9a8248314d68
weian wang <weian.wang@meridigen.com>
2016-10-28 16:01:00 +0800

    cellc2 v07 fix

--- hidos-django/hidos/cellc2/bin/cellc2_V07.py
+++ hidos-django/hidos/cellc2/bin/cellc2_V07.py
@@ -5,29 +5,29 @@ Created on Tue Jun 21 16:02:34 2016
 @author: Aaron.Lin
 """
 
-#get_ipython().magic(u'matplotlib qt')    
+#get_ipython().magic(u'matplotlib qt')
 import cv2
 import numpy as np
 from skimage import filters, morphology, feature
 import json
-import time
-from os import listdir
-import matplotlib.pyplot as plt
-from skimage import io
+# import time
+# from os import listdir
+# import matplotlib.pyplot as plt
+# from skimage import io
 from skimage import img_as_ubyte
 import pymorph as m
 from skimage import exposure
 
-    
-version = '0.7'    
+
+version = '0.7'
 ###############################################################################
 def img_resize(img,max_size):
     len1 = img.shape[0]
     len2 = img.shape[1]
-    
+
     if(len1<max_size and len2<max_size):
         return img
-    
+
     if (len1>=len2):
         factor = float(max_size)/len1
         img_out = cv2.resize(img,(int(factor*len2),max_size))
@@ -39,13 +39,13 @@ def img_resize(img,max_size):
 
 ###############################################################################
 def cellC2_validation(image_input_path):
-    
+
     img_ori=cv2.imread(image_input_path)
-    img_gray=np.zeros_like(img_ori[:,:,0])    
+    img_gray=np.zeros_like(img_ori[:,:,0])
     mask=np.where(img_ori[:,:,0]>200)
     img_gray[mask]=1
     confluence = 100*float(img_gray.sum())/img_gray.size
-    
+
     return confluence
 
 ###############################################################################
@@ -60,12 +60,12 @@ def contrast_saturated(img,alpha):
 
 
 def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
-    
+
 #    tStart = time.time()
-    
-    img_ori=cv2.imread(image_input_path)    
+
+    img_ori=cv2.imread(image_input_path)
     img_ori_resize=img_resize(img_ori,1024)  #resize
-     
+
     #20160804處理全白的scale bar
     mask0=np.where(img_ori[:,:,0]>200)
     mask1=np.where(img_ori[:,:,1]>200)
@@ -86,8 +86,8 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 
     #如果先resize再取gray~效果會比較差
     img_gray=cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
-    img_gray=img_resize(img_gray,1024)  #resize  
-    
+    img_gray=img_resize(img_gray,1024)  #resize
+
     ##################################################
     alpha=1
     img_tmp1=feature.canny(img_gray)
@@ -102,17 +102,17 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
         img_gray=np.copy(img_gray_tmp)
 #    plt.figure()
 #    io.imshow(img_gray)
-#    io.show()   
+#    io.show()
     ##################################################
 
     kernel=np.array([[1,1,1],[1,1,1],[1,1,1]],np.ubyte)
     kernel2=np.array([[0,1,0],[1,1,1],[0,1,0]],np.ubyte)
     kernel3=np.array([[1,1],[1,1]],np.ubyte)
-#    img_gray=filters.rank.enhance_contrast(img_gray,kernel2)    
+#    img_gray=filters.rank.enhance_contrast(img_gray,kernel2)
     img_prewitt=filters.prewitt(img_gray)    #detect edge
 #    plt.figure()
 #    io.imshow(img_prewitt)
-#    io.show()    
+#    io.show()
 #    binarization
     img_enhance=img_as_ubyte(img_prewitt)
     img_enhance = cv2.GaussianBlur(img_enhance,(5,5),0)
@@ -122,34 +122,34 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
     img_roberts=filters.roberts(img_gray)    #detect edge
 #    plt.figure()
 #    io.imshow(img_prewitt)
-#    io.show()    
+#    io.show()
 #    binarization
     img_enhance=img_as_ubyte(img_roberts)
     img_enhance = cv2.GaussianBlur(img_enhance,(5,5),0)
     thresh,img_thresh=cv2.threshold(img_enhance,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     img_thresh_2=img_enhance>=thresh
-      
-    img_thresh_3=feature.canny(img_gray) 
-    
+
+    img_thresh_3=feature.canny(img_gray)
+
     img_thresh=np.uint8(np.logical_or(img_thresh_1,img_thresh_2))
     img_thresh=np.uint8(np.logical_or(img_thresh,img_thresh_3))
-    
-    if float(tmp2)/tmp1>2: 
-       img_thresh=cv2.erode(img_thresh,kernel3,iterations=1) 
+
+    if float(tmp2)/tmp1>2:
+       img_thresh=cv2.erode(img_thresh,kernel3,iterations=1)
        img_thresh=cv2.dilate(img_thresh,kernel,iterations=2)
-    
+
 #    plt.figure()
 #    io.imshow(img_thresh*255)
-#    io.show()  
-        
+#    io.show()
+
     #print thresh
 #    if (thresh<5):
 #        kernel = np.ones((2,2),np.uint8)
-#        img_thresh = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel)  #remove salt 
-            
+#        img_thresh = cv2.morphologyEx(img_thresh, cv2.MORPH_OPEN, kernel)  #remove salt
+
     img_dilation=cv2.dilate(img_thresh,kernel,iterations=2)
 #    img_dilation=cv2.dilate(img_dilation,kernel2,iterations=1)
-#    img_dilation=cv2.dilate(img_dilation,kernel3,iterations=1) 
+#    img_dilation=cv2.dilate(img_dilation,kernel3,iterations=1)
 ##    plt.figure()
 ##    io.imshow(img_dilation)
 ##    io.show()
@@ -158,24 +158,24 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
     img_erosion=cv2.erode(img_erosion,kernel3,iterations=1)
     img_opening = cv2.morphologyEx(img_erosion, cv2.MORPH_OPEN, kernel)  #remove salt
 #    img_opening=cv2.erode(img_opening,kernel)
-    
+
 #    img_opening=np.uint8(np.logical_or(img_thresh,img_opening))
 #    img_opening=cv2.dilate(img_opening,kernel3,iterations=1)
-#    img_opening=cv2.erode(img_opening,kernel3,iterations=1)    
-    
+#    img_opening=cv2.erode(img_opening,kernel3,iterations=1)
+
 #    plt.figure()
 #    io.imshow(img_opening*255)
-#    io.show()    
+#    io.show()
 
- 
+
     if (max(img_opening.shape)>=1024 and float(img_opening.sum())/img_opening.size>0.7):
 #    if (max(img_opening.shape)>=1024):
 #    if (max(img_opening.shape)>=1024 and float(img_opening.sum())/img_opening.size>0.8):
         if float(img_opening.sum())/img_opening.size>0.8:
-            img_opening=cv2.dilate(img_opening,kernel2) 
+            img_opening=cv2.dilate(img_opening,kernel2)
             minimum_size=25-int(np.round(float(img_opening.sum())/img_opening.size*10))*2
         else:
-            minimum_size=5     
+            minimum_size=5
 #        minimum_size=10
 #        if (minimum_size<10):
 #            minimum_size=10
@@ -187,8 +187,8 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #            img_opening=morphology.remove_small_holes(img_opening,min_size=10*10)
 
 
-    
-    ###################################################    
+
+    ###################################################
     mask1=np.where(img_opening>0)
     mask2=np.where(img_opening==0)
     img_foreground=img_gray[mask1].mean()
@@ -218,9 +218,9 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #        else:
 #            img_opening=morphology.remove_small_holes(img_opening,min_size=50)
 
-    ###################################################    
-    
-    
+    ###################################################
+
+
     #############remove isolated small hole############
     radius=30
     disk=np.array(m.sedisk(radius),np.ubyte)
@@ -235,7 +235,7 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
                 tmp1=max(centroids[i,1]-radius,0)
                 tmp2=min(centroids[i,1]+radius+1,img_tmp.shape[0])
                 tmp3=max(centroids[i,0]-radius,0)
-                tmp4=min(centroids[i,0]+radius+1,img_tmp.shape[1])             
+                tmp4=min(centroids[i,0]+radius+1,img_tmp.shape[1])
                 mask_area=np.multiply(disk,img_tmp[tmp1:tmp2,tmp3:tmp4]).sum()
 #                print("mask_area=%d,label_area=%d"%(mask_area,stats[i,4]))
                 if (mask_area==stats[i,4]):
@@ -247,35 +247,35 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
     #######################################
     if (float(img_opening.sum())/img_opening.size>0.8):
         img_opening=cv2.erode(np.uint8(img_opening),kernel)
-        img_opening=cv2.erode(np.uint8(img_opening),kernel3) 
+        img_opening=cv2.erode(np.uint8(img_opening),kernel3)
     else:
-        img_opening=cv2.erode(np.uint8(img_opening),kernel)        
-    
-#    img_opening = cv2.morphologyEx(img_opening, cv2.MORPH_OPEN, kernel)  #remove salt               
-       
+        img_opening=cv2.erode(np.uint8(img_opening),kernel)
+
+#    img_opening = cv2.morphologyEx(img_opening, cv2.MORPH_OPEN, kernel)  #remove salt
+
     mask=np.where(img_opening>0)
     img_ori_resize[:,:,2][mask]=200
 
     #compute confluence
     confluence = 100*float(img_opening.sum())/img_opening.size
 #    ratio=confluence
-    confluence = '%.2f' % confluence 
+    confluence = '%.2f' % confluence
     ### embed result to image
     font =cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img_ori_resize,str(confluence+'%'),(40,80), font, 2,(255,0,0),3)  
-    
+    cv2.putText(img_ori_resize,str(confluence+'%'),(40,80), font, 2,(255,0,0),3)
+
     img_ori_resize = cv2.resize(img_ori_resize,(img_ori.shape[1],img_ori.shape[0]))
     cv2.imwrite(image_output_path,img_ori_resize)
-    
+
     out_file = open(json_path,"w")
-    confluence_result = {'confluence': confluence}    
-    
+    confluence_result = {'confluence': confluence}
+
     json.dump(confluence_result,out_file)
-    out_file.close()    
-    
+    out_file.close()
+
 #    tEnd = time.time()
-#    print ("It costs %f sec",tEnd-tStart)    
-    
+#    print ("It costs %f sec",tEnd-tStart)
+
     return #ratio
 #####################for single image
 
@@ -286,7 +286,7 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 ##
 ##file_list = listdir(image_path)
 ##filename = file_list[7]
-##filename=image_path +'\\'+filename         
+##filename=image_path +'\\'+filename
 #cellConfluence_singleTask(image_path,image_output_path,json_path)
 
 ##############################################################
@@ -308,11 +308,11 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #img_output_path=u'D:\Aaron workspace\Aaron\CellConfluence_Project\output_test'
 ##img_output_path=u'D:\\Aaron workspace\\Aaron\\Benchmark\\Cell C2\\cellc2_result'
 #file_list = listdir(img_input_path)
-#for filename in file_list:  
+#for filename in file_list:
 #    if (filename[0]!='.'):
 ##        print('Cell confluence analyzing for %s'%filename)
 #        print('%s'%filename)
-#        ######################################        
+#        ######################################
 #        inx=filename.rfind('_')
 #        ans_tmp=filename[inx+1:inx+4]
 ##        ground_truth=float(ans_tmp)/10
@@ -326,7 +326,7 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #        result=np.append(result,[[ground_truth,ratio]],axis=0)
 #        print('T=%f, P=%f'%(ground_truth,ratio))
 #
-#result[0,:]=[0,0]        
+#result[0,:]=[0,0]
 #slope, intercept, r_value, p_value, std_err = stats.linregress(result[:,1],result[:,0])
 #print 'p_value=%f'%p_value
 #plt.figure()
@@ -341,11 +341,11 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #plt.xlabel('CellC2',fontsize=14)
 #plt.ylabel('Ground truth',fontsize=14)
 #plt.text(5, 80,('y=%f+%f*x'%(intercept,slope)),fontsize=14)
-#plt.text(5,72,'correlation coefficient=%f'%r_value,fontsize=14)           
+#plt.text(5,72,'correlation coefficient=%f'%r_value,fontsize=14)
 
-##        
-################################################################################        
-        
+##
+################################################################################
+
 #################BBBC for validation
 #from scipy import stats
 ##img_input_path=u'D:\Aaron workspace\Aaron\CellConfluence_Project\CellC MSC Images'
@@ -354,7 +354,7 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #img_output_path=u'D:\\Aaron workspace\\Aaron\\Benchmark\\Cell C2\\cellc2_result2'
 #file_list = listdir(img_input_path)
 #result=np.empty([1,2])
-#for filename in file_list:  
+#for filename in file_list:
 #    if (filename[0]!='.'):
 #        print('Cell confluence analyzing for %s'%filename)
 #        Img_filename=img_input_path+'\\'+filename
@@ -362,15 +362,15 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #        Img_output_filename=img_output_path+'\\'+filename
 #        inx=Img_output_filename.rfind('.')
 #        json_filename=Img_output_filename[0:inx]+'.json'
-#        
+#
 #        inx=Img_filename2.rfind('.')
-#        Img_filename2=Img_filename2[0:inx]+'.png' 
-#        
-#        
-#        ratio=cellConfluence_singleTask(Img_filename,Img_output_filename,json_filename) 
+#        Img_filename2=Img_filename2[0:inx]+'.png'
+#
+#
+#        ratio=cellConfluence_singleTask(Img_filename,Img_output_filename,json_filename)
 #        ground_truth=cellC2_validation(Img_filename2)
 #        result=np.append(result,[[ground_truth,ratio]],axis=0)
-#result[0,:]=[0,0]        
+#result[0,:]=[0,0]
 #slope, intercept, r_value, p_value, std_err = stats.linregress(result[1:,1],result[1:,0])
 #print 'p_value=%f'%p_value
 #plt.figure()
@@ -383,10 +383,9 @@ def cellConfluence_singleTask(image_input_path, image_output_path, json_path):
 #plt.xlabel('CellC2',fontsize=14)
 #plt.ylabel('Ground truth',fontsize=14)
 #plt.text(5, 80,('y=%f+%f*x'%(intercept,slope)),fontsize=14)
-#plt.text(5,72,'correlation coefficient=%f'%r_value,fontsize=14)           
+#plt.text(5,72,'correlation coefficient=%f'%r_value,fontsize=14)
 
-        
+
 ##########################################
 
 
-        
