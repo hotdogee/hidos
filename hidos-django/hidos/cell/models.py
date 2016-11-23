ad8621bac0d9a828ac8eefaa448abaf9a2a8fa96
weian wang <weian.wang@meridigen.com>
2016-11-23 15:55:43 +0800

    forget about permissions, permission should be set in the settings with variable "FILE_UPLOAD_PERMISSION"

--- hidos-django/hidos/cell/models.py
+++ hidos-django/hidos/cell/models.py
@@ -102,7 +102,7 @@ class SingleImageUploadManager(models.Manager):
         obj = super(SingleImageUploadManager, self).create(**data)
         obj.content = obj
 
-        chmod(path.dirname(path.join(settings.MEDIA_ROOT, app_name, 'task', task_id, task_id)), Perm.S_IRWXU | Perm.S_IRWXG | Perm.S_IRWXO)
+        # chmod(path.dirname(path.join(settings.MEDIA_ROOT, app_name, 'task', task_id, task_id)), Perm.S_IRWXU | Perm.S_IRWXG | Perm.S_IRWXO)
         # save uploaded image data to file
         obj.uploaded_image.save(uploaded_image_path, file, save=False)
 
